cae150c0a059a77f0af17309d1c1ffc2025b8ad2
zjuwispersure <534207987@qq.com>
2024-12-03 21:46:41 +0800

    Initial Commit

---
+++ Shared/FlashLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashLightApp.swift
+//  Shared
+//
+//  Created by chengshuo zhang on 2024/12/3.
+//
+
+import SwiftUI
+
+@main
+struct FlashLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
